cf01c70bdd20c1e15edc10a54f93e43a982d65af
Elizaveta <lisa-bella97@mail.ru>
2016-11-30 19:50:48 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <boost/process/context.hpp>
+#include <boost/process/operations.hpp>
+#include <boost/process.hpp>
+#include <iostream>
+#include <boost/regex.hpp>
+
+namespace bp = boost::process;
+
+bp::child start_child(std::string path)
+{
+    bp::context ctx;
+    ctx.stdout_behavior = bp::capture_stream();
+    ctx.environment = bp::self::get_environment();
+
+    return bp::launch(path, std::vector<std::string>(1), ctx);
+}
+
+int main(int argc, char ** argv)
+{
+    if (argc != 2)
+        return 0;
+
+    bp::child child = start_child(argv[1]);
+
+    bp::pistream & is = child.get_stdout();
+
+    std::string line;
+    boost::regex pattern("\\S+\\s*");
+    boost::smatch matches;
+    auto words_number = 0;
+
+    while (std::getline(is, line))
+    {
+        while (boost::regex_search(line, matches, pattern))
+        {
+            words_number += matches.size();
+            line = matches.suffix().str();
+        }
+    }
+
+    std::cout << "The number of words: " << words_number << std::endl;
+
+    bp::status status = child.wait();
+
+    return status.exited() ? status.exit_status() : EXIT_FAILURE;
+}
